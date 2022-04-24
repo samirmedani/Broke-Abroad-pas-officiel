234f372233fae67ddb8d8b84362f606392e8f58f
Mayuu_ <40952685+samirmedani@users.noreply.github.com>
2022-04-25 01:05:42 +0200

    Responsive de la bannière avec le Menu Burger identique au site de officiel

--- js/script.js
+++ js/script.js
@@ -1,17 +1,1 @@
 
-
-function responsiveBurger() {
-
-    (window).on('resize', function() {
-        var win = $(this);
-        if (win.width() > 450) {
-            console.log("test");
-            $('#burger-nav').removeClass('invisible-burger');
-      
-        } else {
-            $('#burger-nav').addClass('visible-burger');
-        }
-      });
-
-}
-
