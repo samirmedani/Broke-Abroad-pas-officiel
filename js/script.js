4ed7c62a947940f62173216450f8aef73326b69d
Mayuu_ <40952685+samirmedani@users.noreply.github.com>
2022-04-26 08:07:42 +0200

    Création d'un carousel en html et javascript

--- js/script.js
+++ js/script.js
@@ -1,1 +1,29 @@
 
+
+
+
+
+
+
+
+
+
+class Carousel {
+
+    constructor
+}
+
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+
+    new Carousel(document.querySelector('#carousel1'), {
+
+        slidesToScroll: 3,
+        slidesVisible: 3
+
+    })
+
+
+})
